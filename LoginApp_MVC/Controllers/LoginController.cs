ce83fe19d18e7d2d8fc5fdff684d7b263f2138d3
Kavinda Dissanayaka <92946@fhss.sjp.ac.lk>
2024-04-21 12:06:42 +0530

    Remove defoult home controler and added Login controller

---
+++ LoginApp_MVC/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoginApp_MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public String Index()
+        {
+            return "Hi Kavinda, THis Is the Login Form You Gona developped";
+        }
+
+    }
+}
